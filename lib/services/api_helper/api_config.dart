abf462c09d192b8fb9b1f7fecdd48d595fc78357
konuleminova <konul.eminova39@gmail.com>
2020-11-24 10:47:38 +0400

    login module done

--- lib/services/api_helper/api_config.dart
+++ lib/services/api_helper/api_config.dart
@@ -1,8 +1,8 @@
 class ApiConfig {
-  final String BASE_URl = 'http://bees.az/api/pad/?lang=ru&action=';
+  final String BASE_URl = 'https://bees.az/api/curyer/?lang=ru&action=';
 
   String LOGIN(String login, String password) =>
-      '${BASE_URl}+login&login=${login}&pass=${password}';
+      '${BASE_URl}login&login=${login}&pass=${password}';
 
   String ORDER_LIST(String token) => '${BASE_URl}orderlist&token=${token}';
 
