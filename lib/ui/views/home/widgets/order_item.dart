f4eaf568c1a5f12231026245ba63e3087494ca16
konuleminova <konul.eminova39@gmail.com>
2020-12-15 12:59:22 +0400

    items kuryer

--- lib/ui/views/home/widgets/order_item.dart
+++ lib/ui/views/home/widgets/order_item.dart
@@ -30,20 +30,16 @@ class OrderItemWidget extends HookWidget {
       statusText = 'I WANT THIS';
     }
     // TODO: implement build
-    return ExpansionTile(
-      initiallyExpanded: valueExpaned.value,
-      trailing: SizedBox(),
-      tilePadding: EdgeInsets.all(0),
-      onExpansionChanged: (v) {
-        valueExpaned.value = v;
-      },
-      title: !valueExpaned.value
-          ? Container(
+    return order.status == 'i want this'?AnimatedCrossFade(
+        firstChild: InkWell(
+            onTap: () {
+              valueExpaned.value = true;
+            },
+            child: Container(
               height: 60.toHeight,
               width: SizeConfig().screenWidth,
               margin: EdgeInsets.only(
-                left: 24.toWidth,
-              ),
+                  left: 16.toWidth, right: 16.toWidth, bottom: 4.toHeight),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.all(Radius.circular(15)),
                 color: Colors.white,
@@ -52,7 +48,10 @@ class OrderItemWidget extends HookWidget {
                 children: [
                   Expanded(
                     child: Container(
-                      child: Text(order.order ?? ''),
+                      child: Text(
+                        order.order ?? '',
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
                       margin: EdgeInsets.only(left: 24.toWidth),
                     ),
                   ),
@@ -70,7 +69,10 @@ class OrderItemWidget extends HookWidget {
                     flex: 1,
                     child: Container(
                       child: Text(
-                        order.finished_time.substring(10) ?? '',
+                        order.finished_time != null &&
+                                order.finished_time.length > 10
+                            ? order.finished_time.substring(10)
+                            : '' ?? '',
                         style: TextStyle(
                             color: ThemeColor().greenColor, fontSize: 12),
                       ),
@@ -94,129 +96,133 @@ class OrderItemWidget extends HookWidget {
                   )
                 ],
               ),
-            )
-          : SizedBox(),
-      children: [
-        InkWell(
-          onTap: (){
-            valueExpaned.value=false;
-          },
-          child: valueExpaned.value?Container(
-              height: 220.toHeight,
-              margin: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-                color: Colors.white,
-              ),
-              child: Column(
+            )),
+        secondChild: InkWell(
+            onTap: () {
+              valueExpaned.value = false;
+            },
+            child: _seconWidget()
+        ),
+        crossFadeState: !valueExpaned.value
+            ? CrossFadeState.showFirst
+            : CrossFadeState.showSecond,
+        duration: Duration(milliseconds: 300)):_seconWidget();
+  }
+
+  _seconWidget() =>Container(
+      height: 220.toHeight,
+      margin: EdgeInsets.only(
+          left: 16.toWidth, right: 16.toWidth, bottom: 4.toHeight),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+        color: Colors.white,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(24.toWidth),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(24.toWidth),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Expanded(
-                              child: ListView.builder(
-                                  shrinkWrap: true,
-                                  // physics: NeverScrollableScrollPhysics(),
-                                  itemCount: order.foods.length,
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    return Container(
-                                        margin:
-                                            EdgeInsets.only(bottom: 4.toHeight),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: <Widget>[
-                                            Text(
-                                              "${index + 1}. ${order.foods[index].data.name ?? ''}",
-                                              style: TextStyle(
-                                                  fontSize: 13.toFont,
-                                                  fontWeight: FontWeight.w500),
-                                              maxLines: 3,
-                                            ),
-                                            SizedBox(
-                                              height: 4.toHeight,
-                                            ),
-                                            Text(
-                                              order.foods[index].data
-                                                      .information ??
-                                                  '',
-                                              style: TextStyle(
-                                                  fontSize: 11.toFont),
-                                            )
-                                          ],
-                                        ));
-                                  })),
-                          SizedBox(
-                            width: 26.toWidth,
-                          ),
-                          Text(
-                            order.order ?? '',
-                            style: TextStyle(fontWeight: FontWeight.w700),
-                          )
-                        ],
-                      ),
-                    ),
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          // physics: NeverScrollableScrollPhysics(),
+                          itemCount: order.foods.length,
+                          itemBuilder:
+                              (BuildContext context, int index) {
+                            return Container(
+                                margin: EdgeInsets.only(
+                                    bottom: 4.toHeight),
+                                child: Column(
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "${index + 1}. ${order.foods[index].data.name ?? ''}",
+                                      style: TextStyle(
+                                          fontSize: 13.toFont,
+                                          fontWeight:
+                                          FontWeight.w500),
+                                      maxLines: 3,
+                                    ),
+                                    SizedBox(
+                                      height: 4.toHeight,
+                                    ),
+                                    Text(
+                                      order.foods[index].data
+                                          .information ??
+                                          '',
+                                      style: TextStyle(
+                                          fontSize: 11.toFont),
+                                    )
+                                  ],
+                                ));
+                          })),
+                  SizedBox(
+                    width: 26.toWidth,
                   ),
-                  InkWell(
-                    onTap: () {
-                      if (order.order != null) {
-                        if (order.status == 'go to rest') {
-                          takeOrder(order.order);
-                        } else if (order.status == 'go to user') {
-                          giveOrder(order.order);
-                        } else if (order.status == 'i want this') {
-                          assignOrder(order.order);
-                        }
-                      }
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(
-                        left: 16,
-                        right: 16,
-                        bottom: 16,
-                      ),
-                      padding: EdgeInsets.all(16.toWidth),
-                      width: SizeConfig().screenHeight,
-                      height: 54.toHeight,
-                      color: statusColor,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            statusText,
-                            style: TextStyle(
-                                fontSize: 19.toFont,
-                                fontWeight: FontWeight.w700,
-                                color: statusColor == ThemeColor().yellowColor
-                                    ? Colors.black
-                                    : Colors.white),
-                          ),
-                          SizedBox(
-                            width: 4.toWidth,
-                          ),
-                          Image.asset(
-                            'assets/images/done.png',
-                            color: statusColor == ThemeColor().yellowColor
-                                ? Colors.black
-                                : Colors.white,
-                            height: 19.toHeight,
-                          )
-                        ],
-                      ),
-                    ),
+                  Text(
+                    order.order ?? '',
+                    style: TextStyle(fontWeight: FontWeight.w700),
                   )
                 ],
-              )):SizedBox(),
-        )
-      ],
-    );
-  }
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              if (order.order != null) {
+                if (order.status == 'go to rest') {
+                  takeOrder(order.order);
+                } else if (order.status == 'go to user') {
+                  giveOrder(order.order);
+                } else if (order.status == 'i want this') {
+                  assignOrder(order.order);
+                }
+              }
+            },
+            child: Container(
+              margin: EdgeInsets.only(
+                left: 16,
+                right: 16,
+                bottom: 16,
+              ),
+              padding: EdgeInsets.all(16.toWidth),
+              width: SizeConfig().screenHeight,
+              height: 54.toHeight,
+              color: statusColor,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    statusText,
+                    style: TextStyle(
+                        fontSize: 19.toFont,
+                        fontWeight: FontWeight.w700,
+                        color: statusColor == ThemeColor().yellowColor
+                            ? Colors.black
+                            : Colors.white),
+                  ),
+                  SizedBox(
+                    width: 4.toWidth,
+                  ),
+                  Image.asset(
+                    'assets/images/done.png',
+                    color: statusColor == ThemeColor().yellowColor
+                        ? Colors.black
+                        : Colors.white,
+                    height: 19.toHeight,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ));
 }
